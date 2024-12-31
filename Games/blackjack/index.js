7f7a4a6240d30cd2795d64e597c2e7bf3ac0dcae
PlaidSnowFrog <riccardobg.coco@gmail.com>
2024-12-31 15:52:30 +0100

    Added some BlackJack Logic

--- Games/blackjack/index.js
+++ Games/blackjack/index.js
@@ -1,3 +1,21 @@
+// Functions
+function hit(cards) {
+    cards.push(Math.floor((Math.random() * 10) + 1));
+}
+
+function displayCards(cards) {
+    let displayString = "";
+
+    for (let i = 0; i < cards.length; i++) {
+        displayString = displayString + cards[i] + "; "
+    }
+
+    return displayString;
+}
+
+// Logic
+
+// Generate cards
 let playerCards = [];
 let dealerCards = [];
 
@@ -9,6 +27,12 @@ for (let i = 0; i < 2; i++) {
     dealerCards[i] = Math.floor((Math.random() * 10) + 1);
 }
 
-function hit(cards) {
-    cards.push(Math.floor((Math.random() * 10) + 1));
-}
+/*
+IDPlayerCards = document.getElementById("playerCards");
+IDDealerCards = document.getElementById("playerCards");
+
+IDPlayerCards = displayCards(playerCards);
+IDDealerCards = displayCards(dealerCards);
+*/
+document.querySelector("#PlayerCards").innerHTML = displayCards(playerCards);
+document.querySelector("#DealerCards").innerHTML = displayCards(dealerCards);
