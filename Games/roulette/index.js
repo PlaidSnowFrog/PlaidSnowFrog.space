57fe634d84c184c08365db559b95d7d1cb6b1130
PlaidSnowFrog <riccardobg.coco@gmail.com>
2025-01-09 14:57:25 +0100

    started working on a roulette

---
+++ Games/roulette/index.js
@@ -0,0 +1,10 @@
+// Functions
+function spin() { // Generate numbers
+    return Math.floor((Math.random() * 32) + 1); // Random number between 1 and 10 (inclusive)
+}
+
+function bet() { //Bet
+    
+}
+
+// Logic
