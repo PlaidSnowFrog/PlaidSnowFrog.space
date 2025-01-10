bae50ac0b170846be3b2242d27666167ee3d4591
PlaidSnowFrog <riccardobg.coco@gmail.com>
2025-01-10 15:11:18 +0100

    im sorry

--- Games/roulette/index.js
+++ Games/roulette/index.js
@@ -9,7 +9,7 @@ function spin() { // Generate numbers
     const num = Math.floor((Math.random() * 37)); // Random number between 0 and 36
 
     // im sorry
-    if (num == 1 || 3 || 5 || 9 || 7 || 12 || 14 || 18 || 16 || 21 || 19 || 23 || 27 || 25 || 30 || 32 || 36 || 34) {
+    if (num == 1 || num == 3 || num ==5 || num ==9 || num ==7 || num ==12 || num ==14 || num ==18 || num ==16 || num ==21 || num ==19 || num ==23 || num ==27 || num ==25 || num ==30 || num ==32 || num ==36 || num ==34) {
         return 1; // red    
     } else if (num == 0) {
         return 0; // green
